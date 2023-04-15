2b1aa7c45e04c40e7759d75313ad15f4676e0fe2
pratikshatrixie <pratikshatrixie@gmail.com>
2023-04-15 10:02:08 +0530

    new commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 app.get("/",(req, res)=>{
     console.log("hello")
     res.json({
-        msg:"Hello from backend message again"
+        msg:"Hello from backend message again. Hello"
     })
 })
 
