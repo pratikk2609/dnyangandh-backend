8fa27e7f8059a91a174421dc183de25caa15210d
sailee2781 <saileekadam03@gmail.com>
2023-04-15 12:01:46 +0530

    added get patch delete and post api

--- app.js
+++ app.js
@@ -18,27 +18,58 @@ app.get("/",(req, res)=>{
         msg:"Hello from backend message again. Hello Team"
     })
 })
-app.get("/user",(req, res)=>{
+
+app.get("/user", async (req, res) => {
     try {
-        reqbody = {
-            username :"pravin",
-            password : "pravin@1234",
-            email : "pravin@gmail.com",
-            age : 45,
-            address : "pashan pune",
-            gender : "Male",
-            phone_number : 123,
-            role : "student"
-        }
-        const adduser = new UserInfo(reqbody);
-        console.log(reqbody);
-        console.log("New User Added in the Database");
-        const newsuser = adduser.save();
-        return res.status(201).send(newsuser );
-      } catch (e) {
-        return res.status(400).send(e);
-      }
+      const getuser = await UserInfo.find({});
+      console.log(getuser)
+      console.log("All the users in the database")
+      return res.send(getuser);
+    } catch (e) {
+      return res.status(400).send(e);
+    }
+  })
+app.post("/user", async (req, res) => {
+    try {
+      const adduser = new UserInfo(req.body)
+      console.log(req.body);
+      console.log("New User Added in the Database");
+      const newuser = await adduser.save();
+      return res.status(201).send(newuser);
+    } catch (e) {
+      return res.status(400).send(e);
+    }
 })
+
+app.delete('/user/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await UserInfo.findByIdAndDelete(id)
+        console.log("Record Deleted")
+        res.send(`Document with ${data.username} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+  })
+
+  app.patch('/user/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+  
+        const user_update = await UserInfo.findByIdAndUpdate(
+            id, updatedData, options
+        )
+        console.log(user_update)
+        console.log("Updated the required the details")
+        res.send(user_update)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+ })
 
 app.listen(5555)
 console.log(`App running on 5555`)
